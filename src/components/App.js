7cadf39360c391c8de8dcc315abee53b2e39da1b
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-12-21 12:13:53 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
+let regeneratorRuntime =  require("regenerator-runtime");
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
